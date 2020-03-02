5e34f995bc14370cb52a6ed42a183ca93ddbb8af
jsjoyy <jsjoyy@126.com>
2020-03-02 12:00:15 +0800

    后台管理框架

--- src/main.js
+++ src/main.js
@@ -9,8 +9,13 @@ import './assets/fonts/iconfont.css'
 Vue.config.productionTip = false
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 Vue.prototype.$http = axios
-
 new Vue({
   router,
   store,
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 Vue.use(VueRouter)
 const routes = [
   {
@@ -13,7 +15,15 @@ const routes = [
     name: 'login',
     component: Login
   },
-  { path: '/home', component: Home },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  },
   {
     path: '/about',
     name: 'About',
@@ -28,17 +38,16 @@ const router = new VueRouter({
   routes
 })
 // 挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   // to 将要去哪个页面
   // from 代表从哪里跳转而来
   // next 是一个函数，表示放行
   //
-  if (to.path === '/login') return next();
+  if (to.path === '/login') return next()
   // 获取当前token
-  const seesionStr = window.sessionStorage.getItem('token');
-  if (!seesionStr)  return next('/login')
-  next ()
-
+  const seesionStr = window.sessionStorage.getItem('token')
+  if (!seesionStr) return next('/login')
+  next()
 })
 
 export default router
